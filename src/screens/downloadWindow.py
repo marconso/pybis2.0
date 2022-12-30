50492e16645ac38f31c2c712429e40ba1b0964f1
marconso <marconsodehsfilho@gmail.com>
2022-12-30 17:48:15 -0400

    funções do botão de seleção servidor e dado

--- src/screens/downloadWindow.py
+++ src/screens/downloadWindow.py
@@ -16,12 +16,41 @@ class WidgetDownload(QWidget):
         super().__init__()
 
         self.drawLayout()
-        data = self.loadConfig(self.confjson)
-        self.cbbxServer.addItems(list(data['SERVER'].keys()))
-        self.cbbxData.addItems(data['SERVER'][self.cbbxServer.currentText()])
+        self.data = self.loadConfig(self.confjson)
+        self.cbbxServer.addItems(sorted(list(self.data['SERVER'].keys())))
+        self.cbbxData.addItems(
+            sorted(self.data['SERVER'][self.cbbxServer.currentText()])
+        )
+        self.cbbxSubData.addItems(
+            sorted(self.data['SERVER']
+            [self.cbbxServer.currentText()]
+            [self.cbbxData.currentText()])
+        )
 
-    def update_items(self):
-        ...
+        self.cbbxServer.currentTextChanged.connect(self.update_items_data)
+        self.cbbxData.currentTextChanged.connect(self.update_items_sub_data)
+
+    def update_items_data(self, server):
+        self.cbbxData.clear()
+        self.cbbxData.addItems(sorted(self.data['SERVER'][server]))
+        try:
+            self.cbbxSubData.addItems(sorted(
+                self.data['SERVER']
+                [self.cbbxServer.currentText()]
+                [self.cbbxSubData.currentText()])
+            )
+        except KeyError:
+            pass
+
+    def update_items_sub_data(self, data):
+        self.cbbxSubData.clear()
+        try:
+            self.cbbxSubData.addItems(sorted(
+                self.data['SERVER']
+                [self.cbbxServer.currentText()][data])
+            )
+        except KeyError:
+            pass
 
     def loadConfig(self, filejson):
         with open(filejson, 'r') as fjson:
@@ -30,7 +59,7 @@ class WidgetDownload(QWidget):
 
     def drawLayout(self):
         group_options_download = QGroupBox('Download options')
-        group_buttons = QGroupBox('Magick')
+        group_buttons = QGroupBox('Run options')
         group_options_pyspark = QGroupBox('Pyspark options')
         group_table = QGroupBox('Table preview')
 
@@ -46,19 +75,19 @@ class WidgetDownload(QWidget):
 
         main_layout = QGridLayout(self)
 
-        self.cbbxServer = QComboBox()
+        self.cbbxServer = QComboBox(editable=True)
 
-        self.cbbxData = QComboBox()
-        self.cbbxSubData = QComboBox()
+        self.cbbxData = QComboBox(editable=True)
+        self.cbbxSubData = QComboBox(editable=True)
 
-        self.cbbxLocal = QComboBox()
-        self.cbbxLocal_ = QComboBox()
+        self.cbbxLocal = QComboBox(editable=True)
+        self.cbbxLocal_ = QComboBox(editable=True)
 
-        self.dtMin = QDateTimeEdit(QDate(2020, 1, 1))
-        self.dtMin.setDateRange(QDate(2020, 1, 1), QDate.currentDate())
+        self.dtMin = QDateTimeEdit(QDate(2010, 1, 1))
+        self.dtMin.setDateRange(QDate(2010, 1, 1), QDate.currentDate())
         self.dtMin.setDisplayFormat('yyyy-MM-dd')
         self.dtMax = QDateTimeEdit(QDate.currentDate())
-        self.dtMax.setDateRange(QDate(2020, 1, 1), QDate.currentDate())
+        self.dtMax.setDateRange(QDate(2010, 1, 1), QDate.currentDate())
         self.dtMax.setDisplayFormat('yyyy-MM-dd')
 
         layout_config_api.addWidget(QLabel('Select server'), 0, 0, 1, 2)
@@ -84,9 +113,9 @@ class WidgetDownload(QWidget):
         self.btnView = QPushButton('Preview')
         self.btnStop = QPushButton('Stop')
 
-        layout_buttons.addWidget(self.btnDownload, 1, 0)
-        layout_buttons.addWidget(self.btnView, 2, 0)
-        layout_buttons.addWidget(self.btnStop, 3, 0)
+        layout_buttons.addWidget(self.btnDownload, 0, 1)#, 2, 1)
+        layout_buttons.addWidget(self.btnView, 0, 2)#, 2, 1)
+        layout_buttons.addWidget(self.btnStop, 0, 3)#, 2, 1)
 
         self.spinCores = QSpinBox()
         self.spinMemory = QSpinBox()
@@ -103,8 +132,8 @@ class WidgetDownload(QWidget):
 
         layout_table.addWidget(self.table, 0, 0)
 
-        main_layout.addWidget(group_options_download, 0, 0)
+        main_layout.addWidget(group_options_download, 0, 0, 1, 0)
 
-        main_layout.addWidget(group_buttons, 0, 1)#, 1, 2)
+        main_layout.addWidget(group_buttons, 1, 0, 1, 0)
 
         main_layout.addWidget(group_table, 2, 0, 1, 2)
